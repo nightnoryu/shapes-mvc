0d054de1793ba4aad5bd0163ddf4a087f1141c6f
m3tro1d <qazwf734@gmail.com>
2022-12-04 22:47:26 +0300

    Add some styling for document view

--- src/view/DocumentView/DocumentView.tsx
+++ src/view/DocumentView/DocumentView.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Settings from '../../model/Settings'
+import styles from './DocumentView.module.css'
 
 function DocumentView(): JSX.Element
 {
     return (
         <svg
             viewBox={`0 0 ${Settings.DOCUMENT_WIDTH} ${Settings.DOCUMENT_HEIGHT}`}
+            className={styles.document}
             tabIndex={0}
             onDragOver={event => event.preventDefault()}
         >
