6e83163246235ac2c51ef0b9d6d472716e8a6ff4
m3tro1d <qazwf734@gmail.com>
2022-12-25 13:16:12 +0300

    Organize tests

--- src/model/test/Editor.test.ts
+++ src/model/test/Editor.test.ts
@@ -1,40 +1,7 @@
-import { EditorInterface } from './EditorInterface'
-import Editor from './Editor'
-import Shape from './Shape'
-import ShapeType from './ShapeType'
-
-describe('shape', () => {
-    const frame = {
-        leftTop: {
-            x: 0,
-            y: 0,
-        },
-        width: 100,
-        height: 100,
-    }
-    const shape = new Shape('fuck', ShapeType.ELLIPSE, frame)
-
-    test('creating a shape', () => {
-        expect(shape.getId()).toEqual('fuck')
-        expect(shape.getType()).toEqual(ShapeType.ELLIPSE)
-        expect(shape.getFrame()).toEqual(frame)
-    })
-
-    test('changing shapes frame', () => {
-        const newFrame = {
-            leftTop: {
-                x: 20,
-                y: 0,
-            },
-            width: 100,
-            height: 1337,
-        }
-
-        shape.setFrame(newFrame)
-
-        expect(shape.getFrame()).toEqual(newFrame)
-    })
-})
+import { EditorInterface } from '../EditorInterface'
+import Editor from '../Editor'
+import Shape from '../Shape'
+import ShapeType from '../ShapeType'
 
 describe('editor', () => {
     let editor: EditorInterface
